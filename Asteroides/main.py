a26d10eddc104415ad14eef0bab5be12425dc4ba
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2023-03-21 22:45:56 -0300

    dia 21 - 03 -2023 adicionei a função Asteroides ounde cria os Asteroides

--- Asteroides/main.py
+++ Asteroides/main.py
@@ -1,4 +1,4 @@
-
+from palette_cors import cors
 import pygame
 import random 
 import math 
@@ -15,8 +15,9 @@ janela = pygame.display.set_mode((largura, altura))
 icon = pygame.image.load('AsTI.png')
 pygame.display.set_icon(icon)
 pygame.display.set_caption('Asteroides')
+fundo = cors('black')
 fundo = pygame.image.load('Menu.png')
-# ----------------------------------------------------
+# ---------------------------------------------------
 
 # Menu + Musicas (Em construção!)
 pygame.mixer.music.load('menu+.wav')
@@ -24,6 +25,12 @@ pygame.mixer.music.set_volume(1.0)
 pygame.mixer.music.play(-1)
 som = True
 som_enter = pygame.mixer.Sound('coin.wav')
+
+# cores
+branco = cors('white')
+
+
+
 
 def menu():
     rodando_menu = True
@@ -48,6 +55,26 @@ menu()
 # Player ( A Fazer)
 
 # Asteroids (A Fazer)
+asteroids = []
+def Asteroids(num_aster = 10):
+    
+    # Crie 10 asteroides aleatórios
+    for i in range(num_aster):
+        # Defina a posição aleatória do asteroide
+        x = random.randint(0, largura)
+        y = random.randint(0, altura)
+        pos = [x, y]
+        
+        # Defina a velocidade aleatória do asteroide no caso de feito em tela
+        veloci_x = random.uniform(0.2 , 0.1)
+        veloci_y = random.uniform(0.2 , 0.1)
+        veloci = [veloci_x, veloci_y]
+        
+        # Defina o tamanho aleatório do asteroide
+        size = random.randint(20, 50)
+        
+        asteroids.append({'pos': pos, 'veloci': veloci, 'size': size})
+    return asteroids
 
 # Colisão  (A Fazer)
 
@@ -63,7 +90,7 @@ menu()
 
 rodando = True
 while rodando:
-    janela.blit(fundo, (0,0))
+    janela.blit(fundo , (0,0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -77,6 +104,15 @@ while rodando:
                  else:
                       pygame.mixer.music.set_volume(0.0)
                       pygame.mixer.music.stop()
+    
+    for asteroid in Asteroids():
+        # Atualize a posição do asteroide de acordo com a sua velocidade
+        asteroid['pos'][0] += asteroid['veloci'][0]
+        asteroid['pos'][1] += asteroid['veloci'][1]
+        
+  
+    pygame.draw.circle(janela, branco, asteroid['pos'], asteroid['size'])    
+
 
     score = 0
     high_score = 0
